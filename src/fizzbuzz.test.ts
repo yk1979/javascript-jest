47b7a506f63c2c72ad8e9850510db15d985b1104
y_irisawa <y_irisawa@waku-2.com>
2022-05-19 22:44:18 +0900

    remove unnecessary test

--- src/fizzbuzz.test.ts
+++ src/fizzbuzz.test.ts
@@ -16,8 +16,5 @@ describe("数列と変換規則を扱う fizzbuzz 関数のテスト", () => {
     test("1を渡すと文字列の`1`が返る", () => {
       assert.strictEqual(fizzbuzz(1), "1");
     });
-    test("2を渡すと文字列の`2`が返る", () => {
-      assert.strictEqual(fizzbuzz(2), "2");
-    });
   });
 });
