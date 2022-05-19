29479be5926088c8493b38b49e41ea8f4bfe22e6
y_irisawa <y_irisawa@waku-2.com>
2022-05-19 22:43:55 +0900

    fix test as a document

--- src/fizzbuzz.test.ts
+++ src/fizzbuzz.test.ts
@@ -1,17 +1,23 @@
 import assert from "assert";
 import { fizzbuzz } from "./fizzbuzz";
 
-describe("FizzBuzz test", () => {
-  test("1を渡すと文字列の`1`が返る", () => {
-    assert.strictEqual(fizzbuzz(1), "1");
+describe("数列と変換規則を扱う fizzbuzz 関数のテスト", () => {
+  describe("3の倍数のときは数の代わりに`Fizz`という文字列が返る", () => {
+    test("3を渡すと`Fizz`が返る", () => {
+      assert.strictEqual(fizzbuzz(3), "Fizz");
+    });
   });
-  test("2を渡すと文字列の`2`が返る", () => {
-    assert.strictEqual(fizzbuzz(2), "2");
+  describe("5の倍数のときは数の代わりに`Buzz`という文字列が返る", () => {
+    test("5を渡すと`Buzz`が返る", () => {
+      assert.strictEqual(fizzbuzz(5), "Buzz");
+    });
   });
-  test("3を渡すと文字列の`Fizz`が返る", () => {
-    assert.strictEqual(fizzbuzz(3), "Fizz");
-  });
-  test("5を渡すと文字列の`Buzz`が返る", () => {
-    assert.strictEqual(fizzbuzz(5), "Buzz");
+  describe("その他の数のときはそのまま文字列に変換する", () => {
+    test("1を渡すと文字列の`1`が返る", () => {
+      assert.strictEqual(fizzbuzz(1), "1");
+    });
+    test("2を渡すと文字列の`2`が返る", () => {
+      assert.strictEqual(fizzbuzz(2), "2");
+    });
   });
 });
