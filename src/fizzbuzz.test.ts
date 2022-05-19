8a32f8da3fb97a108cf3ed38d3ccc743638ddcf5
y_irisawa <y_irisawa@waku-2.com>
2022-05-19 22:31:37 +0900

    fizz buzz step2

--- src/fizzbuzz.test.ts
+++ src/fizzbuzz.test.ts
@@ -8,4 +8,10 @@ describe("FizzBuzz test", () => {
   test("2を渡すと文字列の`2`が返る", () => {
     assert.strictEqual(fizzbuzz(2), "2");
   });
+  test("3を渡すと文字列の`Fizz`が返る", () => {
+    assert.strictEqual(fizzbuzz(3), "Fizz");
+  });
+  test("5を渡すと文字列の`Buzz`が返る", () => {
+    assert.strictEqual(fizzbuzz(5), "Buzz");
+  });
 });
--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,3 +1,5 @@
 export const fizzbuzz = (num: number): string => {
+  if (num % 3 === 0) return "Fizz";
+  if (num % 5 === 0) return "Buzz";
   return num.toString();
 };
